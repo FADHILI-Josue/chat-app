ae0d980acdd3e2fb7a71edbc969606015304b5c9
FADHILI Josue <fadhilijosue27@gmail.com>
2023-05-08 18:22:40 +0200

    adding other login or signup options

---
+++ src/app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { FC, ReactNode } from 'react'
+import { IconType } from 'react-icons'
+
+interface AuthSocialButtonProps {
+  icon:IconType,
+  onClick?: ()=>void,
+  children: ReactNode
+}
+
+const AuthSocialButton: FC<AuthSocialButtonProps> = ({icon:Icon, onClick, children}) => {
+  return <button type='button' onClick={onClick} className='inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0'>
+    <Icon/>
+    {children}
+  </button>
+}
+
+export default AuthSocialButton
--- src/app/(site)/components/Authform.tsx
+++ src/app/(site)/components/Authform.tsx
@@ -1,8 +1,11 @@
 "use client"
-import Input from '@/app/components/Input';
+import Button from '@/app/components/Button';
+import Input from '@/app/components/input/Input';
+import {BsGithub} from 'react-icons/bs'
 import { type } from 'os';
 import { FC, useCallback, useState } from 'react'
 import {SubmitHandler,useForm, FieldValues} from 'react-hook-form'
+import AuthSocialButton from './AuthSocialButton';
 
 interface AuthformProps {
   
@@ -47,8 +50,29 @@ const Authform: FC<AuthformProps> = ({}) => {
   return <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
     <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className=' space-y-6' onSubmit={handleSubmit(onSubmit)}>
+            {variant === 'REGISTER'&& <Input id='name' label='Name' register={register} errors={errors}/>}
+            <Input id='email' label='Email' type='email' register={register} errors={errors}/>
+            <Input id='password' label='Password' type='password' register={register} errors={errors}/>
         <Input id='email' label='Email' register={register} errors={errors}/>
         </form>
+        <div className="mt-4">
+            <Button type='submit' disabled={loading} fullWidth className='hover:scale-[1.03] active:scale-[0.98] bg-gradient-to-tr from-blue-500 to-rose-500'>
+                {variant === 'LOGIN'? 'Sign in':'Register'}
+            </Button>
+        </div>
+        <div className="mt-6">
+            <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                    <div className="w-full border-t border-gray-300"/>
+                </div>
+                <div className="relative flex justify-center text-sm">
+                    <span className=' bg-white px-2 text-gray-500'>Or continue with</span>
+                </div>
+            </div>
+            <div className="mt-6 flex gap-2">
+                <AuthSocialButton icon={BsGithub}>hello</AuthSocialButton>
+            </div>
+        </div>
     </div>
   </div>
 }
